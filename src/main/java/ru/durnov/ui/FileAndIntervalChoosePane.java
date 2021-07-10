10231055e8910520bd9ba6eee1a5e5e63ebeff15
alexdurnoff <door1975@yandex.ru>
2021-07-11 01:38:14 +0300

    Оказывается, можно в узел данные сохранять!!!

--- src/main/java/ru/durnov/ui/FileAndIntervalChoosePane.java
+++ src/main/java/ru/durnov/ui/FileAndIntervalChoosePane.java
@@ -11,20 +11,27 @@ import java.util.Optional;
 public class FileAndIntervalChoosePane {
     private final TextField fileTextFiled = new TextField();
     private final TextField intervalTextField = new TextField();
-    private File file;
-    private DateInterval dateInterval;
 
     public FileAndIntervalChoosePane(){
         Button fileButton = new Button("Выбрать файл для выгрузки");
         fileButton.setOnAction(ae ->{
             FileChooser fileChooser = new FileChooser();
-            file = fileChooser.showSaveDialog(new Stage());
+            File file = fileChooser.showSaveDialog(new Stage());
             fileTextFiled.setText(file.getAbsolutePath());
+            fileTextFiled.setUserData(file);
         });
         Button intervalButton = new Button("Выбрать интервал для выгрузки");
         intervalButton.setOnAction(ae -> {
             Optional<DateInterval> optional = new DataIntervalChooserDialog().showAndWait();
-            optional.ifPresent(interval -> dateInterval = interval);
+            optional.ifPresent(interval -> intervalTextField.setUserData(optional.get()));
         });
+    }
+
+    public File file(){
+        return (File) this.fileTextFiled.getUserData();
+    }
+
+    public DateInterval dateInterval(){
+        return (DateInterval) this.intervalTextField.getUserData();
     }
 }
